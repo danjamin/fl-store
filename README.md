d8ff40ded8e31d669f2e10ac9a52f5498fb59d1e
danjamin <e051ba3be@gmail.com>
2015-06-18 21:39:19 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # fl-store
+
 re-usable store for fluxlike applications
+
+## USE
+
+### install
+
+Note: this is still in development and not registered to **npm** or **bower** yet.
+      So keep this in mind and install via the Git URL for now.
+
+via [bower](http://bower.io):
+
+```sh
+$ bower install https://github.com/danjamin/fl-store.git#master
+```
+
+OR via [npm](http://npmjs.com):
+
+```sh
+$ npm install https://github.com/danjamin/fl-store.git#master
+```
+
+
+## DEVELOP
+
+### requirements
+
+You are going to need node.  Also, install bower, and karma globally:
+
+```sh
+$ npm install -g bower karma-cli
+```
+
+### install dependencies
+
+```sh
+$ npm install
+$ bower install
+```
+
+### linting and testing
+
+```sh
+$ npm test
+```
+
+note: to have karma watch for changes
+
+```sh
+$ karma start
+```
+
+note: to just run the linter
+
+```sh
+$ npm run-script lint
+```
+
+### todo
+
+- [ ] clean up code
+- [ ] Add more unit tests
+- [ ] Bring in flow
+- [ ] Setup travis CI
---
+++ index.js
@@ -0,0 +1,124 @@
+(function() {
+  'use strict';
+
+  var CHANGE_EVENT = 'change',
+    Store,
+    _callbacksExistByName,
+    _on,
+    _emit,
+    _removeChangeListener;
+
+  // Checks for existence of callbacks by name on this
+  // particular Store.
+  _callbacksExistByName = function (eventName) {
+    return (typeof this._callbacks !== 'undefined' && this._callbacks.hasOwnProperty(eventName));
+  };
+
+  // "this" is the particular Store in question
+  // returns boolean -- true if callback added, else false
+  _on = function (eventName, callback) {
+    var callbacks,
+      index;
+
+    // set the callbacks on this Store if not already present
+    if (typeof this._callbacks === 'undefined') {
+      this._callbacks = {};
+    }
+
+    // set the particular set of events by name (if not already present)
+    if (!this._callbacks.hasOwnProperty(eventName)) {
+      this._callbacks[eventName] = [];
+    }
+
+    callbacks = this._callbacks[eventName];
+
+    // find index of callback
+    index = callbacks.indexOf(callback);
+
+    // if callback not present, then add it
+    if (index === -1) {
+      callbacks.push(callback);
+      return true;
+    }
+
+    return false;
+  };
+
+  // "this" is the particular Store in question
+  // returns number, the number of callbacks invoked
+  _emit = function (eventName) {
+    var callbacks,
+      numInvocations = 0;
+
+    // get out if no callbacks exist
+    if (!_callbacksExistByName.call(this, eventName)) {
+      return numInvocations;
+    }
+
+    callbacks = this._callbacks[eventName];
+
+    // invoke each callback by this name
+    callbacks.forEach(function (callback) {
+      callback();
+      ++numInvocations;
+    });
+
+    return numInvocations;
+  };
+
+  // "this" is the particular Store in question
+  // returns boolean of whether or not callback was removed
+  _removeChangeListener = function (eventName, callback) {
+    var callbacks,
+      index;
+
+    // get out if no callbacks exist
+    if (!_callbacksExistByName.call(this, eventName)) {
+      return false;
+    }
+
+    callbacks = this._callbacks[eventName];
+
+    // find index of callback
+    index = callbacks.indexOf(callback);
+
+    // if found, remove it
+    if (index > -1) {
+      callbacks.splice(index, 1);
+      return true;
+    }
+
+    return false;
+  };
+
+  Store = {
+    emitChange: function () {
+      // returns the number of invocations
+      return _emit.call(this, CHANGE_EVENT);
+    },
+
+    addChangeListener: function (callback) {
+      // return boolean, true if added, else false
+      return _on.call(this, CHANGE_EVENT, callback);
+    },
+
+    removeChangeListener: function (callback) {
+      // returns boolean, true if removed, else false
+      return _removeChangeListener.call(this, CHANGE_EVENT, callback);
+    }
+  };
+
+  if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(function () {
+			return Store;
+		});
+	} else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = Store;
+	} else {
+    if (typeof window.fl === 'undefined') {
+      window.fl = {};
+    }
+    window.fl.Store = Store;
+	}
+})();
